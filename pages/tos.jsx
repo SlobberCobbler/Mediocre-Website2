7a321629b189334f39a4e1bf27ea51fe955be2b1
SlobberCobbler <65059918+SlobberCobbler@users.noreply.github.com>
2024-08-12 05:33:45 -0600

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -2,7 +2,7 @@ export default function Tos() {
   return (
     <div>
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
-      <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
+      <p className="text-sm text-white/30 mb-1">Last Update: 14/10/2023 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
 <p>⊳ You cannot use bot commands to harm the bot.</p>
 <div className="py-3"></div>
