28726e9be5e96519ee0aaec89bfc5c7a3c290f4e
SlobberCobbler <65059918+SlobberCobbler@users.noreply.github.com>
2024-08-12 05:32:51 -0600

    Update tos.jsx

--- pages/tos.jsx
+++ pages/tos.jsx
@@ -4,17 +4,13 @@ export default function Tos() {
      <h2 className="mt-4 text-white font-bold text-3xl">Terms of Services</h2>
       <p className="text-sm text-white/30 mb-1">Last Update: 20/05/2022 🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-<p>⊳ Every discord server can use the MusicMaker bot for free, it is completely prohibited to sell or market for a fee.</p>
-<div className="py-3"></div>
 <p>⊳ You cannot use bot commands to harm the bot.</p>
 <div className="py-3"></div>
-<p>⊳ You may not use open source codes to harm other servers.</p>
-<div className="py-3"></div>
-<p>⊳ You may not sell or market open source codes through other platforms.</p>
+<p>⊳ Abusing some commands in the bot will results in a bot ban and you will no longer be able to use any command without appealing.</p>
 <div className="py-3"></div>
 <p>⊳ While using the bot, you cannot use it in a way that harms other users for troll purposes.</p>
 <div className="py-3"></div>
-<p>⊳ To use certain discord bot commands, you need to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot at certain intervals.</p>
+<p>⊳ To use certain discord bot commands, you need to purchase premium for your server to access them!</p>
 </div>
     </div>
   );
