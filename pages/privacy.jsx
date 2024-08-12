11d46bce685388aaa7d32cece4cda7f6b7dfbd0a
SlobberCobbler <65059918+SlobberCobbler@users.noreply.github.com>
2024-08-12 05:12:18 -0600

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,17 +5,29 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for MusicMaker</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the MusicMaker bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ <a className="text-white">WHAT DATA DO WE STORE?</a></p>
         <div className="py-3"></div>
-        <p>⊳ You have to vote for the <a className="text-white" href="https://top.gg/bot/774043716797071371/vote">MusicMaker</a> bot when using certain commands from the bot commands.</p>
+        <p>We do not collect any personal information relating your account like Passwords or any other Credentials. The data we collect includes the User IDs, Server IDs, Channel IDs and Some Role IDs. The bot never requests for any personal data of the users and any act that breaks the Tos of Discord is discouraged by us!</p>
         <div className="py-3"></div>
-        <p>⊳ Every user can use the MusicMaker bot however they want, as long as they do not intend to harm.</p>
+        <p>⊳ <a className="text-white">WHY WE NEED THIS DATA?</a></p>
         <div className="py-3"></div>
-        <p>⊳ You cannot use certain commands at the same time while using MusicMaker.</p>   
+        <p>The data is required for the proper functioning of the bot features like Warning System, Logging and Autoroles. Without this data, our bot will not be able to perform these activities and thus making the features inaccessible for users</p>
         <div className="py-3"></div>
-        <p>⊳ MusicMaker bot does not save any data based on users.</p>
+        <p>⊳ <a className="text-white">HOW DO WE USE THIS DATA?</a></p>
         <div className="py-3"></div>
-        <p>⊳ While using the bot, your user ID is saved and deleted in the system for 60 seconds for once.</p> 
+        <p>The data is used for the proper functioning for the Warning System, Logging activities and Autorole features of our Bot. User IDs are used to identify the users, Channel IDs are used to send the messages to the desginated channels and Server IDs to identify the Servers and the Role IDs are used for the Autorole feature</p>
+        <div className="py-3"></div>
+        <p>⊳ <a className="text-white">HOW LONG DO WE STORE YOUR DATA?</a></p>
+        <div className="py-3"></div>
+        <p>The data is stored as long as the bot is in your Server and as soon as the bot is kicked or removed from the Server, the data is deleted and is no longer to accessable to anyone</p>
+        <div className="py-3"></div>
+        <p>⊳ <a className="text-white">WHO DO WE SHARE THE DATA WITH?</a></p>
+        <div className="py-3"></div>
+        <p>We never share your data with anyone except for <a className="text-white" href="https://auth.mongodb.com/">MongoDB Inc</a> whom is a DataBase Company and all the data for our Bot is stored on the MongoDB Servers and According to MongoDB Inc. the data is only accessable by us.</p>
+        <div className="py-3"></div>
+        <p>⊳ <a className="text-white">GOT CONCERNS OR QUERIES?</a></p>
+        <div className="py-3"></div>
+        <p>If you have any concerns or queries relating our privacy policy or our bot or if you want your data to be removed, you can contact <a className="text-white" href="https://discord.com/users/706760101209309236">slobbercobbler</a> directy on our <a className="text-white" href="https://discord.gg/8ReXz7MUus">Support Server</a>!</p>
     
         </div>
             </div>
